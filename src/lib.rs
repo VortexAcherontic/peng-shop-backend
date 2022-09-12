15e602de27a88cbc467e8b327bdb3f8abf105a3b
Vortex Acherontic <vortex@z-ray.de>
2022-09-12 16:16:16 +0200

    Richer callback

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,47 @@
+pub enum Transaction{
+    NotImplemented,
+    Success,
+    NotFound(Vec<String>),
+    TimeOut
+}
+
+pub fn add_repo(url:String, name:String) -> Transaction{
+    return Transaction::NotImplemented;
+}
+
 /*
 Installs a set of packages
 */
-pub fn install(packages:Vec<String>) -> bool{
-    println!("Not implemented yet!");
-    return true;
+pub fn install(packages:Vec<String>) -> Transaction{
+    return Transaction::NotImplemented;
 }
 
 /*
 Removes a given set of packages.
 */
-pub fn uninstall(packages:Vec<String>) -> bool{
-    println!("Not implemented yet!");
-    return true;
+pub fn uninstall(packages:Vec<String>) -> Transaction{
+    return Transaction::NotImplemented;
 }
 
 /*
 Updates the package cache
 */
-pub fn refresh() -> bool{
-    println!("Not implemeted yet!");
-    return true;
+pub fn refresh() -> Transaction{
+    return Transaction::NotImplemented;
 }
 
 /*
 Performs a system update (not a full upgrade to a new release of the distribution)
 */
-pub fn upgrade() -> bool{
-    println!("Not implemeted yet!");
-    return true;
+pub fn upgrade() -> Transaction{
+    return Transaction::NotImplemented;
 }
 
 /*
 Updates the system to the next available major release.
  */
-pub fn distribution_upgrade() -> bool{
-    println!("Not implemeted yet!");
-    return true;
+pub fn distribution_upgrade() -> Transaction{
+    return Transaction::NotImplemented;
 }
 
 fn has_binary(binary:&str) -> bool{
@@ -54,11 +60,11 @@ mod tests {
         let mut packages_uninstall:Vec<String> = Vec::new();
         packages_uninstall.push("value".to_string());
 
-        assert_eq!(install(packages_install), true);
-        assert_eq!(uninstall(packages_uninstall), true);
-        assert_eq!(upgrade(), true);
-        assert_eq!(refresh(), true);
-        assert_eq!(distribution_upgrade(), true);
+        assert_eq!(install(packages_install), Transaction::NotImplemented);
+        assert_eq!(uninstall(packages_uninstall), Transaction::NotImplemented);
+        assert_eq!(upgrade(), Transaction::NotImplemented);
+        assert_eq!(refresh(), Transaction::NotImplemented);
+        assert_eq!(distribution_upgrade(), Transaction::NotImplemented);
         //let result = add(2, 2);
         //assert_eq!(result, 4);
     }
