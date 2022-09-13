07fe62944380ea57e84097fe1e27a3a909a1f8c4
Vortex Acherontic <vortex@z-ray.de>
2022-09-13 19:26:10 +0200

    add headless detection, su tool detection

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::env;
 
 pub fn is_unix() -> bool {
     return cfg!(unix);
@@ -18,4 +19,52 @@ pub fn has_binary(binary:&str) -> bool{
         }
     }
     return false;
+}
+
+pub fn find_sudo_tool() -> String {
+    let headless_su_tool = ["su", "sudo"];
+    let headed_su_tools = ["pkexec", "xdg-su", "kdesu", "gnome-su"];
+
+    if os_is_headless() {
+        for s in headless_su_tool {
+            if has_binary(s) {
+                return s.to_string();
+            }
+        }
+    } else {
+        for s in headed_su_tools {
+            if has_binary(s) {
+                return s.to_string();
+            }
+        }
+    }
+
+    return "".to_string();
+}
+
+pub fn os_is_headless() -> bool {
+    if is_unix() {
+        let xdg_session_key = "XDG_SESSION_TYPE";
+        let display_servers = ["x11", "wayland"];
+        match env::var(xdg_session_key) {
+            Ok(val) => {
+                for s in display_servers {
+                    if s.to_lowercase() == val.to_lowercase() {
+                        return false;
+                    }
+                }
+                if val == "tty" {
+                    return true;
+                }
+            },
+            Err(e) => {
+                println!("couldn't interpret {}: {}", xdg_session_key, e)
+            }
+        }
+    } else {
+        //Headless Winblows, good joke
+        //This isn't a thing right?
+        return false;
+    }
+    return true;
 }
